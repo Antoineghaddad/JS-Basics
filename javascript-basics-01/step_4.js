bca295a9f6697d65762b29123ba2e4de28626576
Antoineghaddad <antoinehd95@gmail.com>
2020-09-28 11:04:28 +0300

    completed javascript-basics-01 step_4

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,4 @@
+var name = prompt("please enter your name: ");
+var surName = prompt ("please enter your last name: ");
+var city = prompt ("please enter your city: ");
+alert("Hello" + " " + name + " " + surName + " " + "from" + " " + city);
