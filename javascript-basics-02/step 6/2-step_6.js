96c3324d27c2378c2e13b2758919f3634e6c28a0
Antoineghaddad <antoinehd95@gmail.com>
2020-09-29 19:33:07 +0300

    completed javascript-basics-02 step 6 edited

--- javascript-basics-02/step 6/2-step_6.js
+++ javascript-basics-02/step 6/2-step_6.js
@@ -8,6 +8,11 @@ document.querySelector("body").addEventListener("mouseover" , function() {
 });
 
     document.querySelector("body").addEventListener("mouseout" , function(){
-        document.querySelector("#image1").src = "images/image1.jpg";});
+        document.querySelector("#image1").src = "images/image1.jpg";
+        document.querySelector("#image2").src = "images/image2.jpg";
+        document.querySelector("#image3").src = "images/image3.jpg";
+        document.querySelector("#image4").src = "images/image4.jpg";
+        document.querySelector("#image5").src = "images/image5.jpg";
+    });
       
     
