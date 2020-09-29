1fb64066c47badfe718236de8babdc7b0f62c0e3
Antoineghaddad <antoinehd95@gmail.com>
2020-09-29 18:20:56 +0300

    completed javascript-basics-02 step 4

--- javascript-basics-02/step 4/2-step_4.js
+++ javascript-basics-02/step 4/2-step_4.js
@@ -1,5 +1,5 @@
-document.querySelector("button").onclick=function(){
-    if(confirm("Do you want to clear all the fields? ") ){
-    document.querySelector("#name").value = "";
-    document.querySelector("#surname").value = "";
-    document.querySelector("#city").value="";}};
+    document.querySelector("button").onclick =function(){
+    document.querySelector("#name").innerHTML = document.querySelector("#name").value="";
+    document.querySelector("#surname").innerHTML = document.querySelector("#surname").value="";
+    document.querySelector("#city").innerHTML = document.querySelector("#city").value="";
+}
